38a5299ab295876f7232da3789622ba61edb21aa
JaneJung <piaojane@daum.net>
2021-03-21 21:15:13 +0900

    Dealing with the Decimal Place let isInt = floor(currentDisplayValue) == currentDisplayValue if !isInt { return }

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -47,9 +47,21 @@ class ViewController: UIViewController {
                 displayLabel.text = numValue
                 isFinishedTypingNumber = false
             } else {
-                displayLabel.text = displayLabel.text! + numValue
+                
+                if numValue == "." {
+                    
+                    guard let currentDisplayValue = Double(displayLabel.text!) else {
+                        fatalError("Cannot convert display label text to a Double")
+                    }
+                    
+                    let isInt = floor(currentDisplayValue) == currentDisplayValue
+                    
+                    if !isInt {
+                        return
+                    }
+                }
             }
-            
+            displayLabel.text = displayLabel.text! + numValue
             
         }
     }
